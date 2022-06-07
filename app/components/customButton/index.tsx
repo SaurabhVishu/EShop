c32bae6637ae1f5d4327adc5769be057fd93597a
codePradeep <pradeep.sharma1@successive.tech>
2022-06-07 15:41:04 +0530

    minor changes

---
+++ app/components/customButton/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View } from "react-native";
+
+interface Screenprops{
+    navigation:any
+}
+const Screen=(props:Screenprops)=>{
+    const {navigation}=props
+    
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default Screen
+
